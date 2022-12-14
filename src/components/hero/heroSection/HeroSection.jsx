bd65d8cd3c9f3969afb889a32ae74845d563d072
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-12-15 01:00:09 +0600

    aa

--- src/components/hero/heroSection/HeroSection.jsx
+++ src/components/hero/heroSection/HeroSection.jsx
@@ -1,7 +1,67 @@
+import { Stack } from "@mui/system";
 import React from "react";
-
+import { CButton, CTypography } from "../../../utility";
+const data = {
+    title: "EXPLORE NFT COLLECTION",
+    details: `A collection of 2525 highly-fashionable NFTs on the ETH Blockchain. Unique, 
+    metaverse-ready, and designed to benefit their holders.`
+}
 export default function HeroSection() {
-    return <div>
+    const { title, details } = data;
+    return (
+        <Stack
+            height={'100%'}
+            alignItems={'center'}
+            justifyContent={'center'}
+        >
+            <CTypography
+                fontFamily="Bakbak One"
+                align={'center'}
+                fontSize={60}
+                pt={'40%'}
+            >
+                {title}
+            </CTypography>
+            <Stack
+                width={700}
+            >
+                <CTypography
+                    fontFamily="Poppins"
+                    align={'center'}
+                    lineHeight={'30px'}
+                    fontSize={'1rem'}
+                >
+                    {details}
+                </CTypography>
+            </Stack>
+            <Stack
+                direction={'row'}
+                justifyContent={'space-between'}
+                spacing={2}
+            >
+                <CButton
+                    backgroundColor={'#21E786'}
+                    color='#141B22'
+                    padding={'5px 18px'}
+                    fontSize={'0.8rem'}
+                    hoverColor={'#fff'}
+                >
+                    connect wallet
+                </CButton>
+                <CButton
+                    backgroundColor={'none'}
+                    color='#fff'
+                    padding={'5px 18px'}
+                    fontSize={'0.8rem'}
+                    hoverColor={'#fff'}
 
-    </div>;
+                >
+                    whitelist now
+                </CButton>
+
+            </Stack>
+
+
+        </Stack>
+    )
 }
