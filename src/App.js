289e3aea68d85bd67f49e7f317c11fc731cd9143
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-12-15 22:37:33 +0600

    remove btn scss

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import { Stack } from "@mui/system";
-import './App.scss';
+import Collection from "./components/collection/Collection";
+import Footer from "./components/footer/Footer";
+import HeroMain from "./components/hero/HeroMain";
+import Story from "./components/story/Story";
+import Work from "./components/work/Work";
+
 function App() {
   return (
     <Stack
       gap={6}
     >
-      <div class="header">CSS-MASK BUTTON HOVER ANIMATION
-        ( Experimental )</div>
-      <div class="button-container-1">
-        <span class="mas">MASK1</span>
-        <button id='work' type="button" name="Hover">MASK1</button>
-      </div>
-
-      <div class="button-container-2">
-        <span class="mas">MASK2</span>
-        <button type="button" name="Hover">MASK2</button>
-      </div>
-
-      <div class="button-container-3">
-        <span class="mas">MASK3</span>
-        <button type="button" name="Hover">MASK3</button>
-      </div>
-
-      <div class="footer">Inspired by <a href="https://tympanus.net/codrops/2016/09/29/transition-effect-with-css-masks/">this</a> codrops article</div>
-
-
+      <HeroMain />
+      <Work />
+      <Collection />
+      <Story />
+      <Footer />
     </Stack>
   );
 }
